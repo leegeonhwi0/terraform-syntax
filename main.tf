dd35ec6bbbf02075a219e8a006dac15e968be1b0
unknown <lght2002002@gmail.com>
2024-03-07 12:33:27 +0900

    modulariztion with NAT1, IGW1

--- main.tf
+++ main.tf
@@ -12,32 +12,6 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# Create a VPC tags.name = "main"
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "main"
-  }
-}
-# Create a public subnet_1
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.10.0/24"
-  availability_zone = "ap-south-1a"
-
-  tags = {
-    Name = "public_subnet_1"
-  }
-}
-# Create a public subnet_2
-resource "aws_subnet" "private_subnet_1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.101.0/24"
-  availability_zone = "ap-south-1a"
-
-  tags = {
-    Name = "public_subnet_2"
-  }
+module "default_custom_vpc" {
+  source = "./custom_vpc"
 }
