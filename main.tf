ce7fa705e55a073586e3e4fd9a282f9e4fbf4e1f
unknown <lght2002002@gmail.com>
2024-03-07 14:20:09 +0900

    작업완료

--- main.tf
+++ main.tf
@@ -11,7 +11,12 @@ terraform {
 provider "aws" {
   region = "ap-south-1"
 }
-
+# 개발환경
 module "default_custom_vpc" {
+  source = "./custom_vpc"
+}
+
+# 운영환경
+module "prd_custom_vpc" {
   source = "./custom_vpc"
 }
