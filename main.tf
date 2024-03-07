95a59568fb6c393200d6fdf478bc7821e1ccf924
unknown <lght2002002@gmail.com>
2024-03-07 12:15:14 +0900

    make module start

--- main.tf
+++ main.tf
@@ -23,8 +23,9 @@ resource "aws_vpc" "main" {
 }
 # Create a public subnet_1
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.10.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "ap-south-1a"
 
   tags = {
     Name = "public_subnet_1"
@@ -32,8 +33,9 @@ resource "aws_subnet" "public_subnet_1" {
 }
 # Create a public subnet_2
 resource "aws_subnet" "private_subnet_1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.101.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.101.0/24"
+  availability_zone = "ap-south-1a"
 
   tags = {
     Name = "public_subnet_2"
