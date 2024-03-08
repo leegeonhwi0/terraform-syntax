0c9f3b0feeb85009411de068485cf21a00bbd954
unknown <lght2002002@gmail.com>
2024-03-08 10:44:14 +0900

    count문을 이용한 데이터검증 반복조건 실습

--- custom_vpc/main.tf
+++ custom_vpc/main.tf
@@ -8,13 +8,16 @@ resource "aws_vpc" "main" {
   }
 }
 # Create a public subnet
+# 퍼블릭 서브넷은 dev 일 때만 만들어져야 한다
 resource "aws_subnet" "public_subnet" {
+  # count 반복문의 특징 0번 반복은 실행이 안된다
+  count             = var.env == "dev" ? 1 : 0
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.10.0/24"
   availability_zone = local.az_a
 
   tags = {
-    Name = "public_subnet_1_${var.env}"
+    Name = "public_subnet_${var.env}"
   }
 }
 # Create a private subnet
@@ -28,14 +31,14 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
-# Create a NAT gateway
-resource "aws_nat_gateway" "NAT_1" {
-  connectivity_type = "private"
-  subnet_id         = aws_subnet.public_subnet.id
-  tags = {
-    Name = "hangaramit_NAT_1_${var.env}"
-  }
-}
+# # Create a NAT gateway
+# resource "aws_nat_gateway" "NAT_1" {
+#   connectivity_type = "private"
+#   subnet_id         = aws_subnet.public_subnet.id
+#   tags = {
+#     Name = "hangaramit_NAT_1_${var.env}"
+#   }
+# }
 
 # Create a Internet gateway
 resource "aws_internet_gateway" "IGW_1" {
@@ -43,5 +46,15 @@ resource "aws_internet_gateway" "IGW_1" {
 
   tags = {
     Name = "hangaramit_IGW_1_${var.env}"
+  }
+}
+
+# Create a NAT gateway
+resource "aws_nat_gateway" "NAT_1" {
+  count             = var.env == "dev" ? 1 : 0
+  connectivity_type = "public"
+  subnet_id         = aws_subnet.public_subnet[0].id
+  tags = {
+    Name = "hangaramit_NAT_1_${var.env}"
   }
 }
